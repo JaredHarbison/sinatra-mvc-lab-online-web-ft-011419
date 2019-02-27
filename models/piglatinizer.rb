1c73683f329cac4df59e08eb25bf8a6d98a8bd39
Jared Harbison <jared.harbison@gmail.com>
2019-02-27 17:32:54 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,6 @@
 class PigLatinizer
 
-  def piglatinize(user_phrase)
+   def piglatinize(user_phrase)
     if user_phrase.include?(" ")
       pig_phrase = []
       user_phrase.split(" ").each do |word|
@@ -12,20 +12,23 @@ class PigLatinizer
     end
   end
 
-  def translator(word)
+   def translator(word)
     non_pig_latin_words = ["i", "I", "me", "Me", "to", "To", "too", "Too", "a", "A", "an", "An", "in", "In", "and", "And", "on", "On"]
     vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-    if non_pig_latin_words.include?(word)
+
+     if non_pig_latin_words.include?(word)
       word + "way"
     elsif vowels.include? word[0]
       word << "ay"
     else
       consonants = ""
-    while !vowels.include?(word[0])
+
+     while !vowels.include?(word[0])
       consonants << word[0]
       word = word.split("")[1..-1].join
     end
-    word + consonants + 'a'
+    word + consonants + 'ay'
+    end
   end
-end
-end 
+
+ end 
